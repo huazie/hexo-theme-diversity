8a2c043b098349ef0071718b7bee0708a906fd76
Huazie <huazie.lgh@gmail.com>
2024-05-20 00:31:14 +0800

    添加ico图片 和 div点击事件

--- source/js/diversity.js
+++ source/js/diversity.js
@@ -1,52 +1,62 @@
-let xPos = 0;
-gsap.timeline()
-    .set('.ring', { rotationY:180, cursor:'grab' })
-    .from('.img', {
-        duration:1.5,
-        y:200,
-        opacity:0,
-        stagger:0.1,
-        ease:'expo'
-    })
-    .add(()=>{
-        $('.img').on('mouseenter', (e)=>{
-        let current = e.currentTarget;
-        gsap.to('.img', {opacity:(i,t)=>(t==current)? 1:0.5, ease:'power3'})
-        })
-        $('.img').on('mouseleave', (e)=>{
-        gsap.to('.img', {opacity:1, ease:'power2.inOut'})
-        })
-    }, '-=0.5')
+(function($){
 
-$(window).on('mousedown touchstart', dragStart);
-$(window).on('mouseup touchend', dragEnd);
+    let xPos = 0;
+    gsap.timeline()
+        .set('.ring', { rotationY:180, cursor:'grab' })
+        .from('.img', {
+            duration:1.5,
+            y:200,
+            opacity:0,
+            stagger:0.1,
+            ease:'expo'
+        })
+        .add(()=>{
+            $('.img').on('mouseenter', (e)=>{
+            let current = e.currentTarget;
+            gsap.to('.img', {opacity:(i,t)=>(t==current)? 1:0.5, ease:'power3'})
+            })
+            $('.img').on('mouseleave', (e)=>{
+            gsap.to('.img', {opacity:1, ease:'power2.inOut'})
+            })
+        }, '-=0.5')
+
+    $(window).on('mousedown touchstart', dragStart);
+    $(window).on('mouseup touchend', dragEnd);
+            
+    function dragStart(e){ 
+        if (e.touches) e.clientX = e.touches[0].clientX;
+        xPos = Math.round(e.clientX);
+        gsap.set('.ring', {cursor:'grabbing'})
+        $(window).on('mousemove touchmove', drag);
+    }
+
+    function dragEnd(e){
+        $(window).off('mousemove touchmove', drag);
+        gsap.set('.ring', {cursor:'grab'});
+    }
+
+    function drag(e){
+        if (e.touches) e.clientX = e.touches[0].clientX;        
+
+        gsap.to('.ring', {
+            rotationY: '-=' +( (Math.round(e.clientX)-xPos)%360 ),
+            onUpdate:()=>{ gsap.set('.img', { backgroundPosition:(i)=>getBackgroundPosition(i) }) }
+        });
+
+        xPos = Math.round(e.clientX);
+    }
+
+    /**
+     * 返回background-position属性，以在每个图像中创建视差滚动效果
+     */
+    function getBackgroundPosition(i){ 
+        return ( 100-gsap.utils.wrap(0,360,gsap.getProperty('.ring', 'rotationY')-180-i*36)/360*500 )+'px 0px';
+    }
+
+    // 为img的div标签添加事件
+    $(document).on('click', '.img', function() {  
+        // 
         
-function dragStart(e){ 
-    if (e.touches) e.clientX = e.touches[0].clientX;
-    xPos = Math.round(e.clientX);
-    gsap.set('.ring', {cursor:'grabbing'})
-    $(window).on('mousemove touchmove', drag);
-}
-
-function dragEnd(e){
-    $(window).off('mousemove touchmove', drag);
-    gsap.set('.ring', {cursor:'grab'});
-}
-
-function drag(e){
-    if (e.touches) e.clientX = e.touches[0].clientX;        
-
-    gsap.to('.ring', {
-        rotationY: '-=' +( (Math.round(e.clientX)-xPos)%360 ),
-        onUpdate:()=>{ gsap.set('.img', { backgroundPosition:(i)=>getBackgroundPosition(i) }) }
     });
 
-    xPos = Math.round(e.clientX);
-}
-
-/**
- * 返回background-position属性，以在每个图像中创建视差滚动效果
- */
-function getBackgroundPosition(i){ 
-    return ( 100-gsap.utils.wrap(0,360,gsap.getProperty('.ring', 'rotationY')-180-i*36)/360*500 )+'px 0px';
-}
+})(jQuery);
