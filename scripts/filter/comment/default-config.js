13a3ef6ff3556c74cb3466e69e214e76ad2eac72
Huazie <huazie.lgh@gmail.com>
2025-04-03 21:00:42 +0800

    Add: Utterances评论加载时新增加载提示

--- scripts/filter/comment/default-config.js
+++ scripts/filter/comment/default-config.js
@@ -10,10 +10,17 @@ hexo.extend.filter.register('theme_inject', injects => {
         element.args[0] = Object.assign({
             configKey: injectName,
             class: injectName,
-            text: injectName
+            text: injectName,
+            showLoading: false
         }, element.args[0]);
 
         const locals = element.args[0];
+        const injectObj = hexo.theme.config[injectName];
+        // 获取评论系统是否展示加载提示配置
+        // 如果配置为true，则表示评论加载时需要展示加载提示
+        if (injectObj && injectObj.loading)
+            locals.showLoading = injectObj.loading;
+
         const config = hexo.theme.config.comments;
         // 默认展示的评论系统
         if (config.active === locals.configKey) {
--- source/js/third-party/comments/utterances.js
+++ source/js/third-party/comments/utterances.js
@@ -1,4 +1,9 @@
 const loadUtterances = () => {
+    const loadingElement = document.getElementById('loading-utterances');
+    // 配色方案刷新，重新加载评论
+    // 加载提示被隐藏，需要再次显示
+    loadingElement.classList.remove('hidden');
+
     // 加载评论模块
     Diversity.utils.loadComments('.utterances-container')
         .then(() => Diversity.utils.getScript(config.utterances.js, {
@@ -11,6 +16,15 @@ const loadUtterances = () => {
             },
             parentNode: document.querySelector('.utterances-container')
         }));
+
+    // Utterances加载
+    window.addEventListener('message', (e) => {
+        if (e.data && e.data.type == 'resize') {
+            // 检测到Utterances iframe发送了消息，且数据中type为resize，
+            // 则认为Utterances评论已加载，隐藏加载提示
+            loadingElement.classList.add('hidden');
+        }
+    });
 }
 
 document.addEventListener('page:loaded', loadUtterances);
